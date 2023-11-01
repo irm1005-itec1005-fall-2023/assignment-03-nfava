61e452c2260d4c8c5f178d64be5965f0ca45524e
nick fava <nicolasfava@cmail.carleton.ca>
2023-11-01 14:16:51 -0400

    added a empty array named correctly

--- script.js
+++ script.js
@@ -44,7 +44,7 @@
 // }
 
 // Initialise an empty array with the variable name todoItems
-
+  let todoItems = [];
 // Function to add a todo to the list
 // It should accept a string as a parameter (text of the todo item)
 // and it should add a new todo item to the todoItems array
